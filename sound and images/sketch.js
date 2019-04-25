aa519e1ed672f2523aa5824c7a944d1995bde5eb
mhsKJiwa <48918807+mhsKJiwa@users.noreply.github.com>
2019-04-25 10:43:05 -0400

    Update sketch.js

--- sound and images/sketch.js
+++ sound and images/sketch.js
@@ -1,12 +1,18 @@
+var voice;
 var img;
 
 function preload() {
+  voice = loadSound('voice.mp3');
   img = createImg('dance.gif');
 }
 
 function setup() {
   // put setup code here
   createCanvas(winodwWidth,windowHeight);
+  
+   voice.setVolume(0.1);
+   voice.play();
+
 }
 
 function draw() {
