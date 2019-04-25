c58efb3e50c6ced736c265e0f1d385e659e84f75
mhsKJiwa <48918807+mhsKJiwa@users.noreply.github.com>
2019-04-25 10:22:45 -0400

    Update sketch.js

--- sound and images/sketch.js
+++ sound and images/sketch.js
@@ -1,7 +1,15 @@
+var img;
+
+function preload() {
+  img = createImg('dance.gif');
+}
+
 function setup() {
   // put setup code here
+  createCanvas(winodwWidth,windowHeight);
 }
 
 function draw() {
   // put drawing code here
+  img.position(100,100);
 }
